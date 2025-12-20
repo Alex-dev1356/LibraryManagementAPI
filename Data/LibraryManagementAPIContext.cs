04d3a2c574e2d84145a75404c0455224d95f656e
Alex-dev1356 <asoliven26@yahoo.com>
2025-12-20 21:33:49 +0800

    Declaring our tables which will be Added to the Database

--- Data/LibraryManagementAPIContext.cs
+++ Data/LibraryManagementAPIContext.cs
@@ -5,9 +5,10 @@ namespace LibraryManagementAPI.Data
     public class LibraryManagementAPIContext : DbContext
     {
         public LibraryManagementAPIContext(DbContextOptions<LibraryManagementAPIContext> options) 
-            :base(options)
-        {
-            
-        }
+            :base(options) { }
+
+        //Declaring our tables and Migrating them to the database
+        public DbSet<Models.Books> Books { get; set; }
+        public DbSet<Models.Author> Authors { get; set; }
     }
 }
