43590380fd7da114d61207a36e7e597504ca9a99
Alex-dev1356 <asoliven26@yahoo.com>
2025-12-23 04:12:36 +0800

    Adding DTO to retrieve the needed info in Both Books and Author Table

---
+++ DTO/BooksWithAuthor.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryManagementAPI.DTO
+{
+    public class BooksWithAuthor
+    {
+        public string Title { get; set; } = string.Empty;
+        public int PublishedYear { get; set; }
+        public string AuthorName { get; set; } = string.Empty;
+        public string AuthorBio { get; set; } = string.Empty;
+    }
+}
