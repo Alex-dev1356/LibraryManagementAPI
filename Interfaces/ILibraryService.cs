cad3e2d8d2ae6a6b86228f5157a4f8e78bedae39
Alex-dev1356 <asoliven26@yahoo.com>
2025-12-28 22:12:13 +0800

    Implementing Service Layer Design Pattern: Creation of Interfaces and Services

---
+++ Interfaces/ILibraryService.cs
@@ -0,0 +1,15 @@
+﻿using LibraryManagementAPI.DTO;
+using LibraryManagementAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementAPI.Interfaces
+{
+    public interface ILibraryService
+    {
+        Task<IEnumerable<BooksWithAuthor>> GetAllBooksAsync();
+        Task<IEnumerable<BooksWithAuthor>> GetBooksByAuthorAsync(int authorId);
+        Task<Books> AddBookAsync(BooksWithAuthor newBook);
+        Task<Books> UpdateBookAsync(int id, BooksWithAuthor newBook);
+        Task<bool> DeleteBookAsync(int id);
+    }
+}
---
+++ Services/LibraryService.cs
@@ -0,0 +1,40 @@
+﻿using LibraryManagementAPI.Data;
+using LibraryManagementAPI.DTO;
+using LibraryManagementAPI.Interfaces;
+using LibraryManagementAPI.Models;
+
+namespace LibraryManagementAPI.Services
+{
+    public class LibraryService : ILibraryService
+    {
+        private readonly LibraryManagementAPIContext _context;
+        public LibraryService(LibraryManagementAPIContext context)
+        {
+            _context = context;
+        }
+        public Task<Books> AddBookAsync(BooksWithAuthor newBook)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteBookAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<BooksWithAuthor>> GetAllBooksAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<BooksWithAuthor>> GetBooksByAuthorAsync(int authorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Books> UpdateBookAsync(int id, BooksWithAuthor newBook)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
